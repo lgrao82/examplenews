bc073782099a1679e75c2641c08981e8a1f0a682
lgrao82 <lgrao82@gmail.com>
2023-09-05 12:27:10 +0530

    first commit

---
+++ src/components/NewsItem/index.js
@@ -0,0 +1,23 @@
+export const card = {
+  borderRadius: "15px",
+  backgroundColor: "rgb(41, 47, 51)",
+  color: "#fff",
+};
+
+export const img = {
+  borderTopLeftRadius: "15px",
+  borderTopRightRadius: "15px",
+};
+
+export const btn = {
+  borderRadius: "6px",
+  fontWeight: "bold",
+  backgroundColor: "#005abb",
+  boxShadow: "0 .125rem .25rem rgba(0, 0, 0, 0.075)",
+  color: "white",
+  borderColor: "#323762b7",
+};
+
+export const text = {
+  color: "#b6b4b4",
+};
